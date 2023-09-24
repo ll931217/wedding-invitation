b7da5d45a1836661635f4457807437c26d08b2e6
Liang-Shih Lin <liangshih.lin@leapin.co>
2023-09-24 10:31:29 +0800

    fix: added parking info

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,32 @@
-import React, { Component } from 'react';
-import Stories from 'react-insta-stories';
-import Sound from 'react-sound';
+import Stories from "react-insta-stories";
 
-import './App.css';
+import "./App.css";
+import ParkingComponent from "./Parking";
 
-export default () => {
+export default function AppComponent() {
   const stories = [
-    'images/1.png',
+    "images/1.png",
     {
-      url: 'images/2.png',
+      url: "images/2.png",
       seeMore: () => {
-        window.location.replace('https://goo.gl/maps/LSN98yg3uEXQZT278')
+        window.location.replace("https://goo.gl/maps/LSN98yg3uEXQZT278");
       },
     },
-  ]
+    {
+      content: () => ParkingComponent(),
+    },
+  ];
+
   return (
     <div>
-      <Sound
-        url="music.mp3"
-        autoLoad={true}
-        loop={true}
-      />
       <Stories
         id="stories"
         stories={stories}
         defaultInterval={5000}
         width={432}
         height="100vh"
-        loop={true}
+        loop={false}
       />
     </div>
   );
-};
+}
